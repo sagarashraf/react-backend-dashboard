7c2e2e333efc5cda7e78e19e4a51b318b3f399a5
sagarashraf <56229377+sagarashraf@users.noreply.github.com>
2024-07-02 14:38:55 +0100

    bsand api

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express = require("express");
+const mysql = require("mysql2/promise");
+require("dotenv").config();
+const cors = require("cors");
+
 const app = express();
 const port = 5000;
 
-// Define a GET route
+// Set up the MySQL connection
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Allow requests from this origin
+    credentials: true, // Allow credentials such as cookies to be sent
+  })
+);
+// Define the GET route for the homepage
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to Backend</h1>");
+});
+
+// Define the GET route for fetching product settings
+app.get("/productsettings", async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM ProductSetting");
+    console.log(rows);
+    res.status(200).json(rows);
+  } catch (error) {
+    console.error("Error executing query", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 // Start the server
